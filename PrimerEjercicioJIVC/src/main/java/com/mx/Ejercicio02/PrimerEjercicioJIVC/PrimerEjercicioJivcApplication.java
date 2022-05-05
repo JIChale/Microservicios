86b7a9e668a6391aa2e8522b68d69158e7ded711
JIChale <velazquezchale@gmail.com>
2022-05-04 23:45:05 -0500

    PrimeroMicroservicio

---
+++ PrimerEjercicioJIVC/src/main/java/com/mx/Ejercicio02/PrimerEjercicioJIVC/PrimerEjercicioJivcApplication.java
@@ -0,0 +1,13 @@
+package com.mx.Ejercicio02.PrimerEjercicioJIVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerEjercicioJivcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerEjercicioJivcApplication.class, args);
+	}
+
+}
---
+++ PrimerEjercicioJIVC/src/test/java/com/mx/Ejercicio02/PrimerEjercicioJIVC/PrimerEjercicioJivcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.Ejercicio02.PrimerEjercicioJIVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerEjercicioJivcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
